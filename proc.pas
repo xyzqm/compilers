e8eebed31aec90b532e9d9c0130eb4eb660da283
Daniel Zhu <danielfangzhu@gmail.com>
2025-11-23 16:45:28 -0800

    add support for fibonacci as well

--- proc.pas
+++ proc.pas
@@ -4,6 +4,13 @@ begin
     else fact := n * fact(n - 1);
 end;
 
+procedure fib(n);
+begin
+    if n <= 1 then fib := 1
+    else fib := fib(n - 1) + fib(n - 2);
+end;
+
 begin
 WRITELN(fact(10));
+WRITELN(fib(10));
 end;
